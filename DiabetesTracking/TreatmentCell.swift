b3fe15d6b5bc404b7dafdefe78b4f85169144ba6
Invincible_Pain <terrorgeek@gmail.com>
2016-03-02 15:53:54 -0500

    a

---
+++ DiabetesTracking/TreatmentCell.swift
@@ -0,0 +1,13 @@
+//
+//  TreatmentCell.swift
+//  DiabetesTracking
+//
+//  Created by Yu on 3/2/16.
+//  Copyright © 2016 EFA. All rights reserved.
+//
+
+import Cocoa
+
+class TreatmentCell: UITableViewCell {
+
+}
